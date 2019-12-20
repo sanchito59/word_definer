fc5beeb3be0db9aa17b68cfe54f3cd4d4ffacb78
Chris Sanchez <c.sanch7@gmail.com>
2019-12-20 09:26:33 -0800

    add failing tests for .clear and #save

--- lib/word.rb
+++ lib/word.rb
@@ -1,2 +1,10 @@
 class Word
+    attr_reader :name, :id
+    @@words = {}
+    @@total_rows = 0
+  
+    def initialize(name, id)
+      @name = name
+      @id = id || @@total_rows += 1
+    end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,3 +1,25 @@
 require 'rspec'
 require 'word'
 require 'definition'
+
+describe '#word' do
+    before(:each) do
+      Word.clear()
+    end
+  
+    describe('.all') do
+      it('returns an empty array when there are no words') do
+        expect(Word.all).to(eq([]))
+      end
+    end
+  
+    describe('#save') do
+      it('saves a word') do
+        word = word.new('languid', nil)
+        word.save()
+        word2 = word.new('jovial', nil)
+        word2.save()
+        expect(Word.all).to(eq([word, word2]))
+      end
+    end
+end
