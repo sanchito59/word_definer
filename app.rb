b5d966e7a29b1c27b20b9d2431489d2291dd88c4
Chris Sanchez <c.sanch7@gmail.com>
2019-12-20 10:30:26 -0800

    update routes and add content to word.erb

--- app.rb
+++ app.rb
@@ -5,9 +5,25 @@ require('./lib/definition')
 also_reload('lib/**/*.rb')
 
 get('/') do
+    @words = Word.sort
     erb(:words)
 end
 
 get('/word/new') do
+    "Hello World"
     erb(:new_word)
 end
+
+get('/words/:id') do
+    @word = Word.find(params[:id].to_i())
+    erb(:word)
+end
+
+post('/words') do
+    values = *params.values
+    word = Word.new(values[0], nil)
+    word.save()
+    @words = Word.sort()
+    erb(:words)
+end
+
