b87a5e809148d84ab6e5045fc1acbc4d41bd179b
Chris Sanchez <c.sanch7@gmail.com>
2019-12-20 08:47:28 -0800

    update Gemfile and bundle it, add first path for app.rb

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source('https://rubygems.org')
 
-ruby '2.5.1'
 gem('sinatra')
 gem('rspec')
 gem('sinatra-contrib')
--- app.rb
+++ app.rb
@@ -3,3 +3,7 @@ require('sinatra/reloader')
 require('./lib/word')
 require('./lib/definition')
 also_reload('lib/**/*.rb')
+
+get('/') do
+    erb(:words)
+end
