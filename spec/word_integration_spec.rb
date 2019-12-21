0ff40b5a917df3a192b6b27f7ef9c56c6e12e5b3
Chris Sanchez <c.sanch7@gmail.com>
2019-12-20 16:32:24 -0800

    update edit_word and add 1/2 passing tests for integration testing, debugging InvalidURIError

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -3,6 +3,11 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
+before(:each) do
+    Word.clear()
+    Definition.clear()
+  end
+
 describe('create an word path', {:type => :feature}) do
     it('creates a word and then goes to the words page') do
     visit('/words')
@@ -22,4 +27,28 @@ describe('create a definition path', {:type => :feature}) do
       click_on('Add definition')
       expect(page).to have_content('a sinful fruit')
     end
-  end
+end
+
+describe('Update \'word\' path', {:type => :feature}) do
+    it('will navigate to the update word page and change the word') do
+        word = Word.new("apple", nil)
+        word.save
+        visit("/words/#{word.id}")
+        click_on('Edit word')
+        fill_in('term', :with => 'peach')
+        click_on('Update Word')
+        expect(page).to have_content('peach')
+    end
+end
+
+describe('Delete word', {:type => :feature}) do
+    it('will navigate to the update word page and delete the word') do
+        Word.clear()
+        word = Word.new("apple", nil)
+        word.save
+        visit("/words/#{word.id}")
+        click_on('Edit word')
+        click_on('Delete Word')
+        expect(page).not_to have_content('apple')
+    end
+end
