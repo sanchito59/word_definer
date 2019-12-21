18aefaa449ef8bc2f1b3fe3c583afc21a977b38e
Chris Sanchez <c.sanch7@gmail.com>
2019-12-20 16:05:20 -0800

    change word.erb to use an <ol>, add first two passing integration specs

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -2,3 +2,24 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('create an word path', {:type => :feature}) do
+    it('creates a word and then goes to the words page') do
+    visit('/words')
+    click_on('Add a new word')
+    fill_in('word', :with => 'canary')
+    click_on('Enter!')
+    expect(page).to have_content('canary')
+    end
+end
+
+describe('create a definition path', {:type => :feature}) do
+    it('creates a definition and then goes to the definition page') do
+      word = Word.new("apple", nil)
+      word.save
+      visit("/words/#{word.id}")
+      fill_in('definition', :with => 'a sinful fruit')
+      click_on('Add definition')
+      expect(page).to have_content('a sinful fruit')
+    end
+  end
